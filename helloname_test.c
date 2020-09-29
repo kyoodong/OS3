16b47a60af64ff7923f88969b3b54c0b2b000102
이동규 <wind.dong.dream@gmail.com>
2020-09-30 02:59:03 +0900

    Init

---
+++ helloname_test.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+
+int main(int argc, char *argv[]) {
+	if (argc < 2)
+		exit();
+
+	hello_name(argv[1]);
+	exit();
+}
